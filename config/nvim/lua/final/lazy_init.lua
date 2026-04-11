0343ffac4f649ff321894666dd67b0afc6801f3a
Alejandro Baldres <alejandro.baldres@gmail.com>
2026-04-11 10:28:36 -0300

    fix:Fix Dependencias Lazy

--- config/nvim/lua/final/lazy_init.lua
+++ config/nvim/lua/final/lazy_init.lua
@@ -77,7 +77,11 @@ require("lazy").setup({
         dependencies = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
-            "L3MON4D3/LuaSnip",
+            {
+                "L3MON4D3/LuaSnip",
+                version = "v2.*",
+                build = "make install_jsregexp",
+            },
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
         },
